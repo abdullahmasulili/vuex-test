82caf33368dd3fabcf0b4f4775461672c4e8d7b9
Abdullah Masulili <abdullahsrcc13@gmail.com>
2021-10-19 09:25:53 +0800

    add HTTP request

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,59 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: []
+  },
+  getters: {
+    allTodos: state => state.todos
   },
   mutations: {
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    removeTodo: (state, id) => state.todos = state.todos.filter( todo => todo.id  !== id),
+    putTodo:(state, upTodo) => {
+      const index = state.todos.findIndex(todo => todo.id === upTodo.id)
+      if(index !== -1){
+        state.todos.splice(index, 1, upTodo)
+      }
+    }
   },
   actions: {
+    async fetchTodos({
+      commit
+    }) {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+      commit('setTodos', response.data)
+      // console.log(response.data)
+    },
+    async addTodo({
+      commit
+    }, todo) {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/todos', todo)
+      console.log(response.data)
+      commit('newTodo', response.data)
+    },
+    async deleteTodo({
+      commit
+    }, id){
+      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      commit('removeTodo', id)
+      console.log(`Todo with id : ${id} Deleted`)
+    },
+    async limitTodos({commit}, limit){
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
+      commit('setTodos', response.data)
+      console.log(response.data)
+    },
+    async updateTodo({commit}, updTodo){
+      const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo)
+      commit('putTodo', response.data)
+      console.log(response.data)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p class="font-weight-bold">List Todos</p>
+      </v-col>
+      <v-col>
+        <v-form>
+          <v-text-field outlined color="green accent-4" label="Title" v-model="title"></v-text-field>
+        </v-form>
+        <v-btn large color="green accent-4" class="float-right white--text" @click="submitTodo">
+          <v-icon left> mdi-plus </v-icon>
+          <span>Add Todo</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p>Limit Results</p>
+        <v-select :items="limit_item" v-model="limit" @change="handleLimit"></v-select>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center">
+      <v-col
+        cols="12"
+        md="6"
+        lg="3"
+        xl="3"
+        v-for="(todo, i) in allTodos"
+        :key="i"
+      >
+        <v-alert
+          border="left"
+          colored-border
+          :color="giveColor(todo.completed)"
+          elevation="2"
+          class="pa-0"
+          @dblclick="handleUpdate(todo)"
+        >
+          <v-card elevation="0">
+            <v-card-title class="d-flex justify-center">
+              <p class="text-center">{{ todo.title }}</p>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <span>ID : {{todo.id}}</span>
+              <v-btn icon color="warning" @click="handleDelete(todo.id)">
+                <v-icon>mdi-trash-can</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapActions, mapGetters } from "vuex";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data(){
+    return{
+      title: null,
+      limit_item: [200, 100, 50, 10, 5],
+      limit: null,
+    }
+  },
+  computed: {
+    ...mapGetters(["allTodos"]),
+  },
+  methods: {
+    ...mapActions(["fetchTodos", "addTodo", "deleteTodo", "limitTodos", "updateTodo"]),
+    submitTodo(){
+      let data = {
+        title: this.title,
+        completed: false
+      }
+      this.addTodo(data)
+      console.log(data)
+    },
+    handleDelete(id){
+      this.deleteTodo(id)
+    },
+    handleLimit(){
+      let val = this.limit
+      console.log(val)
+      this.limitTodos(val)
+    },
+    handleUpdate(item){
+      const data = {
+        id: item.id,
+        title: item.title,
+        completed: !item.completed
+      }
+      this.updateTodo(data)
+      // console.log(item)
+    },
+    giveColor(completed){
+      if(!completed){
+        // console.log('false')
+        return "red accent-3"
+      } else {
+        // console.log('true')
+        return "green accent-3"
+      }
+    }
+  },
+  components: {},
+  created() {
+    this.fetchTodos();
+  },
+};
 </script>
